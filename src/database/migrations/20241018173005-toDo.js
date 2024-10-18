7862c06fb6d9fdff2df6b5c139c6487ea34f91ab
Jemilson-Luis <jemilsonluis@gmail.com>
2024-10-18 16:31:01 -0300

    created migrations users and to-dos

---
+++ src/database/migrations/20241018173005-toDo.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('tasks', { 
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'users',
+          key: 'id'
+        },
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE'
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      desc: {
+        type: Sequelize.TEXT,
+        allowNull: false
+      }
+
+    });
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('tasks');
+    
+  }
+};
